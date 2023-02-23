ec05707ea02a4ad8dd489f88777f52c9f87bc5f3
Janos Tordai <tjanos88@gmail.com>
2023-02-23 21:54:14 +0100

    DEV: added functionality for clear, undo and dot buttons

--- js/main.js
+++ js/main.js
@@ -13,10 +13,12 @@ let multiplyButton = document.querySelector("#multiply");
 let subtractButton = document.querySelector("#subtract");
 let addButton = document.querySelector("#add");
 let equalButton = document.querySelector("#equal");
+let clearButton = document.querySelector("#clear");
+let undoButton = document.querySelector("#undo");
+let dotButton = document.querySelector("#dot");
 let mainDisplay = document.querySelector(".main-display");
 let secondaryDisplay = document.querySelector(".secondary-display");
 
-// equalButton.addEventListener("click", operate(numbers))
 
 numericButtons.forEach(button => {
     button.addEventListener(
@@ -40,12 +42,41 @@ allPrintableButtons.forEach(button => {
         })
 })
 
-equalButton.onclick = (e) => operationOrEqualPushed(e.target.value);
 
+equalButton.onclick = (event) => operationOrEqualPushed(event.target.value);
+clearButton.onclick = () => clearDisplayAndMemory();
+undoButton.onclick = () => undoLastInput();
+dotButton.onclick = (event) => numericButtonPushed(event.target.value);
+
+
+function clearDisplayAndMemory() {
+    clearOperation();
+    clearNumbers();
+    clearUserNumberArray();
+    clearMainDisplay("0");
+    clearSecondaryDisplay();
+}
+
+function undoLastInput() {
+    if (userNumberStrings.length > 0) {
+        userNumberStrings.pop();
+        removeLastCharacterFromMain();
+    }
+    else {
+        clearSecondaryDisplay();
+    }
+}
+
+function removeLastCharacterFromMain() {
+    let currentMainDisplayText = mainDisplay.innerHTML;
+    mainDisplay.innerHTML = currentMainDisplayText.substring(0, currentMainDisplayText.length - 1);
+}
 
 function numericButtonPushed(value) {
+    if (userNumberStrings.length === 0 && mainDisplay.innerHTML === "0") {
+        mainDisplay.innerHTML = "";
+    }
     if (userNumberStrings.length === 0 && numbers.length === 0) {
-        clearMainDisplay();
         userNumberStrings.push(value);
     } else if (userNumberStrings.length === 0 && numbers.length === 1) {
         userNumberStrings.push(value);
@@ -55,18 +86,36 @@ function numericButtonPushed(value) {
 }
 
 function addToMainDisplay(value) {
-    mainDisplayValue += value;
-    mainDisplay.innerHTML += value;
+    if (value === "." && mainDisplay.innerHTML.indexOf('.') !== -1) {
+        if (numbers.length !== 1) {
+            return
+        } else {
+            mainDisplayValue += value;
+            mainDisplay.innerHTML += value;
+        }
+    } else {
+        mainDisplayValue += value;
+        mainDisplay.innerHTML += value;
+    }
 }
 
-function clearMainDisplay() {
+function clearMainDisplay(defaultValue) {
     mainDisplayValue = "";
-    mainDisplay.innerHTML = "";
+    mainDisplay.innerHTML = defaultValue;
+}
+
+function clearSecondaryDisplay() {
+    secondaryDisplayValue = "";
+    secondaryDisplay.innerHTML = "";
 }
 
 function addToSecondaryDisplay(value) {
     secondaryDisplayValue = value;
     secondaryDisplay.innerHTML = value;
+}
+
+function clearOperation() {
+    userOperation = "";
 }
 
 function operationOrEqualPushed(operation) {
@@ -76,7 +125,7 @@ function operationOrEqualPushed(operation) {
         let result = operate(numbers, userOperation);
         clearNumbers();
         addToSecondaryDisplay(mainDisplay.innerHTML + "=");
-        clearMainDisplay();
+        clearMainDisplay("");
         addToMainDisplay(result);
         userOperation = operation;
     } else if (operation === "=" && numbers.length === 1 && userNumberStrings.length === 0) {
@@ -85,11 +134,14 @@ function operationOrEqualPushed(operation) {
         let result = operate(numbers, userOperation);
         clearNumbers();
         addToSecondaryDisplay(mainDisplay.innerHTML + "=");
-        clearMainDisplay();
+        clearMainDisplay("");
         addToMainDisplay(result);
         userOperation = operation;
     } else if (operation === "=" && numbers.length === 0) {
         return
+    } else if (operation !== "=" && userOperation !== "" && numbers.length === 1) {
+        userOperation = operation;
+        removeLastCharacterFromMain();
     } else if (operation !== "=" && userNumberStrings.length > 0) {
         userOperation = operation;
         numbers.push(concatenateNumber(userNumberStrings));
@@ -99,13 +151,29 @@ function operationOrEqualPushed(operation) {
         userOperation = operation;
         addToMainDisplay("0");
     } else if (operation !== "=" && userNumberStrings.length === 0 && numbers.length === 0 && userOperation === "=") {
-        numbers.push(parseInt(mainDisplayValue));
+        numbers.push(convertStringToNumber(mainDisplayValue));
         userOperation = operation;
     }
 }
 
 function concatenateNumber(userNumberStrings) {
-    return parseInt(userNumberStrings.join(""));
+    return convertStringsArrayToNumber(userNumberStrings);
+}
+
+function convertStringsArrayToNumber(stringArray) {
+    if (stringArray.includes(".")) {
+        return round(parseFloat(stringArray.join("")), 2);
+    } else {
+        return parseInt(stringArray.join(""));
+    }
+}
+
+function convertStringToNumber(text) {
+    if (text.indexOf('.') !== -1) {
+        return round(parseFloat(text), 2);
+    } else {
+        return parseInt(text);
+    }
 }
 
 function clearUserNumberArray() {
@@ -117,19 +185,19 @@ function clearNumbers() {
 }
 
 function add(numbers) {
-    return numbers[0] + numbers[1]
+    return round(numbers[0] + numbers[1], 2);
 }
 
 function subtract(numbers) {
-    return numbers[0] - numbers[1]
+    return round(numbers[0] - numbers[1], 2);
 }
 
 function multiply(numbers) {
-    return numbers[0] * numbers[1]
+    return round(numbers[0] * numbers[1], 2);
 }
 
 function divide(numbers) {
-    return round(numbers[0] / numbers[1], 2)
+    return round(numbers[0] / numbers[1], 2);
 }
 
 function operate(numbers, operation) {
